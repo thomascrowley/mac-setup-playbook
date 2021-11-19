b218640cb9360e1e41d79f3ca00b3cb592fd3d9d
Thomas Crowley <15927917+thomascrowley@users.noreply.github.com>
2021-11-19 10:36:37 +0000

    Replace makefile with bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set up ZSH
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+ln -sfn ~/.dotfiles/zshrc ~/.zshrc
+ln -sfn ~/.dotfiles/vimrc ~/.vimrc
+ln -sfn ~/.dotfiles/p10k.zsh ~/.p10k.zsh
+ln -sfn ~/.dotfiles/gitconfig ~/.gitconfig
+
+# Install fonts
+git clone --branch=20160623-Stable https://github.com/antijingoist/open-dyslexic.git ~/opendyslexic
